bee99ea5d509b86d1f804a7a4055e2c68c6b2afd
JeremyJMoss <mossyj19@gmail.com>
2022-07-24 21:33:13 +1000

    404.html added for all paths not caught by server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,26 +1,32 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const server = express();
+const path = require("path");
 
 let port = process.env.PORT || 5000;
 
 server.use(bodyParser.json());
 
 server.use((req, res, next) => {
+    res.setHeader
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     next();
 })
 
-server.get("/", (req, res) => {
+server.get("/hello-world", (req, res) => {
     res.send("<h1>Hello World!</h1>")
 })
 
 server.post("/food", (req, res) => {
     console.log(req.body);
     console.log("You hit the server");
-    res.json(req.body).send();
+    res.json(req.body).status(200).send();
+})
+
+server.use((req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "views", "404.html"));
 })
 
 server.listen(port, () => {
